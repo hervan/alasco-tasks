c8114579beb34134853b3f72fef7b65b3878cab7
Hervan Rossi <hervan@gmail.com>
2018-12-16 13:15:08 -0200

    new geometric classes, refactoring of rectangle methods and tests

--- rectangles.js
+++ rectangles.js
@@ -3,16 +3,54 @@ class Point {
     this.x = x;
     this.y = y;
   }
-}
 
-class Rectangle {
-  constructor(points) {
-    this.points = points;
+  isCongruent(point2) {
+    return this.x == point2.x && this.y == point2.y;
   }
 }
 
-let rectanglesIntersect = (rectangle1, rectangle2) => false;
+class Segment {
+  constructor(point1, point2) {
+    this.point1 = point1;
+    this.point2 = point2;
+  }
+
+  determinant() {
+    return this.point1.x * this.point2.y - this.point2.x * this.point1.y;
+  }
+
+  length() {
+    return Math.pow(this.point2.x - this.point1.x, 2)
+    + Math.pow(this.point2.y - this.point1.y, 2);
+  }
+
+  formsRightAngle(segment2) {
+    return this.point2.isCongruent(segment2.point1)
+    && Math.pow(this.length(), 2) + Math.pow(segment2.length(), 2) ==
+      Math.pow((new Segment(segment2.point2, this.point1)).length(), 2);
+  }
+}
+
+class Rectangle {
+  constructor(segments) {
+    this.segments = segments;
+  }
+
+  area() {
+    return this.segments.reduce((acc, segment) => acc + segment.determinant() / 2, 0);
+  }
+
+  isValid() {
+    return this.area() > 0
+    && this.segments.length == 4
+    && this.segments.every((segment, i) => segment.formsRightAngle(this.segments[(i + 1) % 4]));
+  }
+
+  intersects(rectangle2) {
+    return false;
+  }
+}
 
 exports.Point = Point;
+exports.Segment = Segment;
 exports.Rectangle = Rectangle;
-exports.rectanglesIntersect = rectanglesIntersect;
--- rectangles.test.js
+++ rectangles.test.js
@@ -1,18 +1,24 @@
-const { Point, Rectangle, rectanglesIntersect } = require('./rectangles');
+const {
+  Point,
+  Segment,
+  Rectangle
+} = require('./rectangles');
+
+const point1 = new Point(1, 1);
+const point2 = new Point(1, 2);
+const point3 = new Point(2, 2);
+const point4 = new Point(2, 1);
+
+const segment1 = new Segment(point1, point2);
+const segment2 = new Segment(point2, point3);
+const segment3 = new Segment(point3, point4);
+const segment4 = new Segment(point4, point1);
+
+const rectangle1 = new Rectangle([segment1, segment2, segment3, segment4]);
+const rectangle2 = new Rectangle([segment1, segment2, segment3, segment4]);
 
 test('rectangles are identical (and therefore intersect)', () => {
   expect(
-    rectanglesIntersect(
-      new Rectangle([
-        new Point(1, 1),
-        new Point(1, 2),
-        new Point(2, 2),
-        new Point(2, 1)
-      ]),
-      new Rectangle([
-        new Point(1, 1),
-        new Point(1, 2),
-        new Point(2, 2),
-        new Point(2, 1)
-      ]))).toBe(true);
+    rectangle1.intersects(rectangle2)
+  ).toBe(true);
 });
