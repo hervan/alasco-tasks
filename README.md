79995f3510d1e86fde3bcec76d4d6fd9a3edcd36
Hervan Rossi <hervan@gmail.com>
2018-12-17 08:19:07 -0200

    documentation (README.md) files for the tasks

---
+++ README.md
@@ -0,0 +1,24 @@
+# Rectangles and Dices
+### created by Hervan Rossi for Alasco selection process
+
+the third task can be found inside the [currency-converter](currency-converter) folder.
+
+## Configuring the project
+
+In the project directory, assuming a fairly recent Node/NPM environment, you must run:
+
+### `npm install`
+
+After a while the project should be configured and ready to run.
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm test`
+
+Runs the test suit for the Rectangles task.
+
+### `node dices.js`
+
+Runs the Dices task with 1000 iterations of the game, to assess whether it's a good game.
--- currency-converter/src/ConversionResult.js
+++ currency-converter/src/ConversionResult.js
@@ -4,7 +4,7 @@ let ConversionResult = (props) =>
   !props.convertedAmount ? null :
   <div className="conversion-result fw btn-lg alert alert-success" role="alert">
     {!props.amount ? "Invalid number." :
-      `${props.currencies[props.currencyFrom].symbol}${props.amount.toFixed(props.currencies[props.currencyFrom].code == "JPY" ? 0 : 2)} is ${props.currencies[props.currencyTo].symbol}${props.convertedAmount.toFixed(props.currencies[props.currencyTo].code == "JPY" ? 0 : 2)}`}
+      `${props.currencies[props.currencyFrom].symbol}${props.amount.toFixed(props.currencies[props.currencyFrom].code === "JPY" ? 0 : 2)} is ${props.currencies[props.currencyTo].symbol}${props.convertedAmount.toFixed(props.currencies[props.currencyTo].code === "JPY" ? 0 : 2)}`}
   </div>;
 
 export default ConversionResult;
